36dd0231aedb8bcec536751626e733f8fa93421a
monstercat16 <tsygankov1357@gmail.com>
2023-02-28 19:53:17 +0300

    aaab

---
+++ lab3.py
@@ -0,0 +1,19 @@
+def a():
+    n=int(input("введите количество слов"))
+    a=''
+    for i in range(n):
+        x=str(input("введите слово"))
+        a=a+x+" "
+    print(a)
+
+a()
+
+def b():
+    a=''
+    while:
+        x = str(input("введите слово"))
+        a = a + x + " "
+        if x=="stop": break
+    print(a)
+
+b()
