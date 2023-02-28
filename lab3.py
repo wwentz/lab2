03a0001fe9656f3bed7401004729292a11bd4633
monstercat16 <tsygankov1357@gmail.com>
2023-02-28 20:05:10 +0300

    aaaаы

--- lab3.py
+++ lab3.py
@@ -10,10 +10,35 @@ a()
 
 def b():
     a=''
-    while:
-        x = str(input("введите слово"))
+    x = str(input("введите слово"))
+    while x!='stop':
         a = a + x + " "
-        if x=="stop": break
+        x = str(input("введите слово"))
     print(a)
 
 b()
+
+def c():
+    x = str(input("введите слово"))
+    if x.count("ф")>0:
+        print('вау редкое слово')
+    else:
+        print('блин слово не редкое')
+
+c()
+
+def d():
+    from random import randint
+
+    x=0
+    while x<3:
+        a=int(randint(0,20))
+        b=int(randint(0,20))
+        print(a,"+",b,"=")
+        y=int(input("введите ответ"))
+        if a+b!=y:
+            x=x+1
+    if x==3:
+        print('игра окончена')
+
+d()
